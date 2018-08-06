169053960f71e57681d75a2158f1851e99fab7f6
pavelp21 <pavelvp2182@yandex.ru>
2018-08-06 23:58:00 +0500

    Why is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,36 @@
 $(document).ready(function() {
+    /* Якорь */
+    $(function () {
+        $("a[href='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
+    /*План*/
+$(function () {
+    $('.day').click('.day_toggle_trigger', function(){
+        if ($('.day_toggle_trigger',this).text() == 'Смотреть программу')
+        {
+            $('.day_body', this).slideDown(600);
+        $('.day_toggle_trigger',this).text('Свернуть');
+    }
+        else{
+            $('.day_body', this).slideUp(600);
+            $('.day_toggle_trigger',this).text('Смотреть программу');
+        }
+    }
+    ,
+// function(){
+//         $('.day_body', this).slideUp(600);
+//         $('.day_toggle_trigger').text('Смотреть программу');
+//     }
 
-
+);
+});
 
 /*Конец документа*/
 });

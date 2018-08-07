9060c062652209b2ceeecacbd181b7c82e9076c0
pavelp21 <pavelvp2182@yandex.ru>
2018-08-07 17:46:01 +0500

    Response is done

--- js/scripts.js
+++ js/scripts.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
             0: {
                 items: 1
             },
-            300: {
+            360: {
                 items: 2
             },
             510: {
@@ -88,24 +88,24 @@ $(document).ready(function () {
         }
     });
 
-        /*Страые браузеры*/
-        var yaBrowserUpdater = new ya.browserUpdater.init({
-            "lang": "ru",
-            "browsers": {
-                "yabrowser": "15.10",
-                "chrome": "52",
-                "ie": "10",
-                "opera": "40",
-                "safari": "8",
-                "fx": "47",
-                "iron": "35",
-                "flock": "Infinity",
-                "palemoon": "25",
-                "camino": "Infinity",
-                "maxthon": "4.5",
-                "seamonkey": "2.3",
-                "theme": "blue"
-            }
-        });
+    /*Страые браузеры*/
+    var yaBrowserUpdater = new ya.browserUpdater.init({
+        "lang": "ru",
+        "browsers": {
+            "yabrowser": "15.10",
+            "chrome": "52",
+            "ie": "10",
+            "opera": "40",
+            "safari": "8",
+            "fx": "47",
+            "iron": "35",
+            "flock": "Infinity",
+            "palemoon": "25",
+            "camino": "Infinity",
+            "maxthon": "4.5",
+            "seamonkey": "2.3",
+            "theme": "blue"
+        }
+    });
     /*Конец документа*/
 });

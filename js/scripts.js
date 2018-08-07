473ab93fd807a4560b3c7d133463e6dca1cf9644
pavelp21 <pavelvp2182@yandex.ru>
2018-08-07 14:39:30 +0500

    Desktop is done

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,43 @@ $(document).ready(function () {
             }
         });
     });
+    /* Таймер */
+    var clock;
+    var futureDate = new Date("August 07, 2018 00:00 AM UTC+3");
+    var currentDate = new Date();
+    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    function dayDiff(first, second) {
+        return (second - first) / (1000 * 60 * 60 * 24);
+    }
+    if (dayDiff(currentDate, futureDate) < 100) {
+        $('.clock').addClass('twoDayDigits');
+    } else {
+        $('.clock').addClass('threeDayDigits');
+    }
+    if (diff < 0) {
+        diff = 0;
+        $('.clock-stop').addClass("alert-danger");
+        $('.clock-stop').html("Скидка уменьшилась!");
+        // $('#sale').text('1 000р.');
+        // $('#today').text('9 900р.');
+        // $('#link').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=1921&clean=true&lg=ru');
+
+    }
+    clock = $('.clock').FlipClock(diff, {
+        clockFace: 'HourlyCounter',
+        countdown: true,
+        language: 'ru',
+        callbacks: {
+            stop: function () {
+                $('.clock-stop').addClass("alert-danger");
+                $('.clock-stop').html("Скидка уменьшилась!");
+                //  $('#sale').text('1 000р.');
+                //  $('#today').text('9 900р.');
+                //  $('#link').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=1921&clean=true&lg=ru');
+            }
+        },
+    });
     /*Сертификаты*/
     $('#certificats').owlCarousel({
         loop: true,
@@ -46,7 +83,7 @@ $(document).ready(function () {
     });
     /*Видео-отзывы Жанны*/
     $('#jannas_videos, #alexs_videos').owlCarousel({
-        loop: true,
+        loop: false,
         margin: 20,
         nav: true,
         navText: ["&larr;", "&rarr;"],
@@ -72,9 +109,29 @@ $(document).ready(function () {
     });
 
     /*Видео в отзывах*/
-    $(".video_wrapper").click('.video_wrapper img', function () {
-        var a = $(this).attr("data-youtube");
-        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
+    $(".video_wrapper img").click( function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
     });
+
+        /*Страые браузеры*/
+        var yaBrowserUpdater = new ya.browserUpdater.init({
+            "lang": "ru",
+            "browsers": {
+                "yabrowser": "15.10",
+                "chrome": "52",
+                "ie": "10",
+                "opera": "40",
+                "safari": "8",
+                "fx": "47",
+                "iron": "35",
+                "flock": "Infinity",
+                "palemoon": "25",
+                "camino": "Infinity",
+                "maxthon": "4.5",
+                "seamonkey": "2.3",
+                "theme": "blue"
+            }
+        });
     /*Конец документа*/
 });

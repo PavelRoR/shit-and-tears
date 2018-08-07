832a223dc06c3000720c67db878b37d4f557a315
pavelp21 <pavelvp2182@yandex.ru>
2018-08-07 13:51:29 +0500

    Reveiws is done

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
             }
         });
     });
-    // owl-carousel
+    /*Сертификаты*/
     $('#certificats').owlCarousel({
         loop: true,
         margin: 0,
@@ -43,6 +43,38 @@ $(document).ready(function () {
                 items: 6
             }
         }
+    });
+    /*Видео-отзывы Жанны*/
+    $('#jannas_videos, #alexs_videos').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        navText: ["&larr;", "&rarr;"],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+    /*Текстовые отзывы Жанны*/
+    $('#jannas_texts, #alexs_texts').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        navText: ["&larr;", "&rarr;"],
+        dots: false,
+        items: 1,
+        autoHeight: true
+    });
+
+    /*Видео в отзывах*/
+    $(".video_wrapper").click('.video_wrapper img', function () {
+        var a = $(this).attr("data-youtube");
+        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
     });
     /*Конец документа*/
 });
